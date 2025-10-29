066f8621b8d8321655d3884cf20c9b203050cb69
Richard Sugiarto <rich.sugiarto@gmail.com>
2025-10-29 12:53:58 +0800

    Modify skills content on skill section

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -17,10 +17,10 @@ function Skills() {
     const Website = [
         { label: 'ReactJS', percentage: '75%' },
         { label: 'JavaScript', percentage: '75%' },
-        { label: 'TypeScript', percentage: '55%' },
-        { label: 'PHP', percentage: '55%' },
-        { label: 'AWS (VPS)', percentage: '50%' },
-        { label: 'Google Cloud (VPS)', percentage: '50%' },
+        { label: 'Docker', percentage: '50%' },
+        { label: 'K8s', percentage: '50%' },
+        { label: 'AWS (EC2 & S3)', percentage: '50%' },
+        { label: 'GCP', percentage: '50%' },
     ];
     return (
         <>
